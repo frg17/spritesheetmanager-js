e62d6585fb037d1601bb7fd6448afa4b0d1143b9
Frosti Grétarsson <gretarssonur@gmail.com>
2018-10-03 23:45:19 +0000

    Bætt við prufu í ssrdev.js

--- ssrdev.js
+++ ssrdev.js
@@ -1,14 +1,16 @@
 let sprite1;
+let sprite2;
 
 window.addEventListener('DOMContentLoaded', () => {
     SpriteSheetRenderer.init("canvas");
 
     SpriteSheetRenderer.createSpriteFromSpriteSheet(
-        "pirate1idle.png",
-        32, 32, 6, (result) => {
-            console.log("Donezel washington");
-            sprite1 = result;
-        }
-    )
-});
+        "pirate1idle.png", 32, 32, 6,
+        (result) => { sprite1 = result;}
+    );
 
+    SpriteSheetRenderer.createSpriteFromSpriteSheet(
+        "heroidle.png", 64, 64, 8, 
+        (result) => { sprite2 = result }
+    );
+});
